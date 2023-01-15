8921a40072fb76bbd318c4b57610c62193ee6c6a
Bryson <48774118+leekycauldron@users.noreply.github.com>
2023-01-15 18:40:53 -0500

    Added a space like background with moving stars

--- Main.java
+++ Main.java
@@ -13,11 +13,36 @@ public class Main extends World
      * Constructor for objects of class Main.
      * 
      */
+    
     public Main()
     {    
         super(400, 600, 1);
+        prepare();
         
+    }
+    
+    public void prepare() {
+        // Set Background to complete darkness.
+        GreenfootImage background = getBackground();
+        background.setColor(Color.BLACK);//Add Background color
+        background.fillRect(0,0,getWidth(),getHeight());
+        
+        
+        // Spawn in the player.
         Player player = new Player(20,2);
         addObject(player,getWidth()/2,getHeight()-75);
+        
+        
+        // Add the stars
+        for (int i = 0; i < 300; i++) {
+            Star star = new Star();
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            addObject(star,x,y);
+        }
+    }
+    
+    public void act() {
+        
     }
 }
